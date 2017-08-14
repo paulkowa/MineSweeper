a093fde60dce2c8b1367cfaefdba378d068a861d
paulkowa <paulkowa@buffalo.edu>
2017-08-14 15:33:00 -0400

    First upload

--- Backend/Threading/Timer.cs
+++ Backend/Threading/Timer.cs
@@ -1,1 +1,13 @@
- 
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    public class Timer : BackgroundWorker
+    {
+    }
+}
