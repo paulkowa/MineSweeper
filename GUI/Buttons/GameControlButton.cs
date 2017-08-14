56cbcb715551a1936339ebed7f41e9a706b54500
paulkowa <paulkowa@buffalo.edu>
2017-08-14 15:34:43 -0400

    First upload

---
+++ GUI/Buttons/GameControlButton.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace MineSweeper.GUI.Butons
+{
+    public class GameControlButton : Button
+    {
+        private Game game;
+        private Image easy, medium, hard, loss, win;
+        public GameControlButton(Game game)
+        {
+            this.game = game;
+            setImages();
+            setProperties();
+        }
+        /// <summary>
+        /// Event handler for left click
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+        }
+        /// <summary>
+        /// Event handler for right click
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseRightButtonDown(e);
+            gameMedium();
+        }
+        /// <summary>
+        /// Method to change appearence upon losing the game
+        /// </summary>
+        public void gameLost()
+        {
+            Content = loss;
+        }
+        /// <summary>
+        /// Method to change appearence upon winning the game
+        /// </summary>
+        public void gameWon()
+        {
+            Content = win;
+        }
+        /// <summary>
+        /// Change content image to easy
+        /// </summary>
+        private void gameEasy()
+        {
+            Content = easy;
+        }
+        /// <summary>
+        /// Change content image to medium
+        /// </summary>
+        private void gameMedium()
+        {
+            Content = medium;
+        }
+        /// <summary>
+        /// Change content image to hard
+        /// </summary>
+        private void gameHard()
+        {
+            Content = hard;
+        }
+
+        private void setDifficultyIcon(Game game)
+        {
+            
+        }
+        /// <summary>
+        /// Set image variables to proper content
+        /// </summary>
+        private void setImages()
+        {
+            easy = new Image();
+            easy.Source = new BitmapImage(new Uri(@"/Images/sleeping.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+            medium = new Image();
+            medium.Source = new BitmapImage(new Uri(@"/Images/unamused.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+            hard = new Image();
+            hard.Source = new BitmapImage(new Uri(@"/Images/fear.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+            loss = new Image();
+            loss.Source = new BitmapImage(new Uri(@"/Images/loss.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+            win = new Image();
+            win.Source = new BitmapImage(new Uri(@"/Images/victory.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+        }
+        /// <summary>
+        /// Set button properties
+        /// </summary>
+        private void setProperties()
+        {
+            Width = 60;
+            Margin = new Thickness(0,3,0,3);
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+            Background = Brushes.DodgerBlue;
+            BorderBrush = Brushes.White;
+            Content = easy;
+        }
+    }
+}
--- GUI/Buttons/MineButton.cs
+++ GUI/Buttons/MineButton.cs
@@ -1,1 +1,113 @@
- 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Input;
+
+namespace MineSweeper.GUI.Buttons
+{
+    public class MineButton : Button
+    {
+        private Game game;
+        private Image flag, mine, cross;
+        private Tile tile;
+        public MineButton(Game game, Tile tile = null)
+        {
+            this.game = game;
+            this.tile = tile;
+            setProperties();
+            setImages();
+        }
+        /// <summary>
+        /// Right click event handler
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseRightButtonDown(e);
+            setFlag();
+        }
+        /// <summary>
+        /// Left click event handler
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            setMine();
+        }
+        /// <summary>
+        /// Double left click event handler
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnMouseDoubleClick(MouseButtonEventArgs e)
+        {
+            base.OnMouseDoubleClick(e);
+        }
+
+        /// <summary>
+        /// Set the content image to a flag
+        /// </summary>
+        private void setFlag()
+        {
+            Content = flag;
+        }
+        /// <summary>
+        /// Set the content to null
+        /// </summary>
+        private void setEmpty()
+        {
+            Content = null;
+        }
+        /// <summary>
+        /// Set the content image to a mine
+        /// </summary>
+        private void setMine()
+        {
+            Content = mine;
+        }
+        /// <summary>
+        /// Set the content image to a cross
+        /// </summary>
+        private void setCross()
+        {
+            Content = cross;
+        }
+        /// <summary>
+        /// Set images to their resources
+        /// </summary>
+        private void setImages()
+        {
+            flag = new Image();
+            flag.Source = new BitmapImage(new Uri(@"/Images/sleeping.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+            mine = new Image();
+            mine.Source = new BitmapImage(new Uri(@"/Images/sleeping.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+            cross = new Image();
+            cross.Source = new BitmapImage(new Uri(@"/Images/sleeping.png", UriKind.RelativeOrAbsolute));
+            VerticalAlignment = VerticalAlignment.Center;
+            HorizontalAlignment = HorizontalAlignment.Center;
+
+        }
+        /// <summary>
+        /// Set default properties
+        /// </summary>
+        private void setProperties()
+        {
+            Height = 29;
+            Width = 29;
+            Background = Brushes.DodgerBlue;
+            BorderBrush = Brushes.White;
+        }
+    }
+}
