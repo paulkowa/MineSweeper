6ba54db8ce4c9de1c0cffb6a0ca9a0376027c534
paulkowa <paulkowa@buffalo.edu>
2017-08-16 16:04:04 -0400

    Add files via upload

--- Interfaces/IBoard.cs
+++ Interfaces/IBoard.cs
@@ -1,1 +1,72 @@
- 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Interfaces
+{
+    public interface IBoard
+    {
+        /*
+         * 
+         * Methods to create a board, and tiles and set tile content
+         * 
+         */
+        void MoveMine(Tile t);
+        /// <summary>
+        /// Get tile at index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index</returns>
+        Tile GetTile(int index);
+        /// <summary>
+        /// Get tile left of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index - 1</returns>
+        Tile GetLeftOf(int index);
+        /// <summary>
+        /// Get tile upper left of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index - (x - 1)</returns>
+        Tile GetUpperLeftOf(int index);
+        /// <summary>
+        /// Get tile upper of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index - x</returns>
+        Tile GetUpperOf(int index);
+        /// <summary>
+        /// Get tile upper right of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index - (x + 1)</returns>
+        Tile GetUpperRightOf(int index);
+        /// <summary>
+        /// Get tile right of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index + 1</returns>
+        Tile GetRightOf(int index);
+        /// <summary>
+        /// Get tile lower right of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index + (x + 1)</returns>
+        Tile GetLowerRightOf(int index);
+        /// <summary>
+        /// Get tile lower of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index + x</returns>
+        Tile GetLowerOf(int index);
+        /// <summary>
+        /// Get tile lower left of index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Tile at index + (x - 1)</returns>
+        Tile GetLowerLeftOf(int index);
+    }
+}
---
+++ Interfaces/IDifficulty.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper.Interfaces
+{
+    public interface IDifficulty
+    {
+        /// <summary>
+        /// Swtich difficulty level to next highest
+        /// </summary>
+        void SwitchDifficulty();
+        /// <summary>
+        /// Get the tile count based on the current difficulty
+        /// </summary>
+        /// <returns>An integer representing the number of tiles</returns>
+        int GetTileCount();
+        /// <summary>
+        /// Get the number of tiles which are mines based on the current difficulty
+        /// </summary>
+        /// <returns>An integer representing the number of tiles which are mines</returns>
+        int GetMineCount();
+    }
+}
