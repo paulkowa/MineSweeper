e50888edbffc2ad0df3feb1ca27e29529e73d1a9
paulkowa <paulkowa@buffalo.edu>
2017-08-17 14:11:03 -0400

    Add files via upload

---
+++ Backend/Threading/TileChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineSweeper
+{
+    public class TileChecker : BackgroundWorker
+    {
+        MainWindow window;
+        public TileChecker(Queue<Tile> tiles)
+        {
+            WorkerReportsProgress = false;
+            WorkerSupportsCancellation = false;
+            RunWorkerCompleted += TileChecker_RunWorkerCompleted;
+            DoWork += TileChecker_DoWork;
+        }
+        private void TileChecker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+        }
+
+        private void TileChecker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+        }
+    }
+}
+
